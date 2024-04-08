2898f83302fc2f1a51bee2c2b4095c9c98ca8d23
thangpdgcd <thangpdgcd201784@fpt.edu.vn>
2024-04-08 17:21:59 +0700

    update file bubblesort

--- Bubblesort_day3.java
+++ Bubblesort_day3.java
@@ -3,22 +3,18 @@ package day3;
 import java.util.Arrays;
 
 public class Bubblesort_day3 {
-
     public void bubbleSort(int[] array) {
-        for (int i = 0; i < array.length -1; i++) {
-            int minIndex = i;
-            for (int j = 0; j < array.length - i -1; j++) {
-                if (array[j] < array[minIndex + 1]) {
-                    minIndex = j;
+        for (int i = 0; i < array.length ; i++) {
+            for (int j = 1; j < array.length ; j++) {
+                if (array[j] < array[j - 1]) {
+                    // swap
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
                 }
             }
-            // swap
-            int temp = array[minIndex+1];
-            array[minIndex+1] = array[i];
-            array[i] = temp;
         }
     }
-
 }
 class runnerbuble{
     public static void main (String[] args)
